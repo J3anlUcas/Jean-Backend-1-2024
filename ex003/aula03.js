9c38bcd1e1ef32297fcf2adb01e45b8332364bf4
JEAN LUCAS <jeanlucas2005@hotmail.com>
2024-03-12 10:58:11 -0300

    Organizando o código

--- ex003/aula03.js
+++ ex003/aula03.js
@@ -1,43 +1,31 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
-app.use(express.json())
-
-var vetMeuvetor = []
+var vetMeuvetor = [];
 
 app.post("/contatos", function (req, res) {
-    var { nome, cep, email } = req.body;
-    const {id} = req.params;
-    
-    vetMeuvetor.push(req.body)
-    
-    console.log(vetMeuvetor);
-    
-    return res.json({
-        id:id,
-        nome: nome,
-        CEP: cep,
-        Email: email
-    })
+  var { nome, cep, email } = req.body;
+  vetMeuvetor.push(req.body);
+
+  return res.json({
+    nome: nome,
+    CEP: cep,
+    Email: email
+  });
 });
-
-app.get("/cadastro",(req,res)=>{
-    res.json(vetMeuvetor)
-})
-
-app.get("/search/:nome",(req,res)=>{
- var {nome} = req.params;
- const foundUser = vetMeuvetor.find((user) => user.nome == nome);
- 
- if (foundUser) {
+app.get("/cadastro", (req, res) => {
+  res.json(vetMeuvetor);
+});
+app.get("/search/:nome", (req, res) => {
+  var { nome } = req.params;
+  const foundUser = vetMeuvetor.find((user) => user.nome == nome);
+  if (foundUser) {
     res.json(foundUser);
   } else {
     res.status(404).json({ message: 'Não há a pessoa na base de dados.' });
   }
-
-
-})
-
+});
 app.listen(3004, () => {
-    console.log("Servidor iniciado na porta 3004!");
+  console.log("Servidor iniciado na porta 3004!");
 });
